5f282c068d28c1d7f70d95c02b3504c0abdc8701
Lee Jen Tun <bmpsst511@gmail.com>
2021-10-25 00:41:30 +0800

    package json update

--- server.js
+++ server.js
@@ -8,11 +8,10 @@
     // Serve only the static files form the dist directory
     app.use(express.static('./dist/angular-heroku'));
 
-    app.get('/*', function(req,res) {
-        
-    res.sendFile(path.join('./dist/angular-heroku/index.html'));
+    app.get('/*', function (req, res) {
+        res.sendFile('index.html', { root: 'dist/angular-heroku' }
+        );
     });
 
     // Start the app by listening on the default Heroku port
     app.listen(process.env.PORT || 8080);
-
